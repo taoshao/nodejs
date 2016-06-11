53ee1589ac2dc864450b3ff9092479715e933b4d
taoshao <995279109@qq.com>
2016-06-11 10:17:12 +0800

    add files

---
+++ file-explorer/index.js
@@ -0,0 +1,70 @@
+var fs = require('fs');
+var stdin = process.stdin;
+var stdout = process.stdout;
+var stats = [];
+// console.log(fs.readdirSync(__dirname));
+fs.readdir(__dirname,function(err,files){
+	console.log(files);
+});
+fs.readdir(process.cwd(),function(err,files){
+	console.log('');
+
+	if (!files.length) {
+		return consoles.log('no files to show');
+	}
+
+	console.log('select which file or directory you want to see');
+	file(0);
+	function file(i){
+	var filename = files[i];
+
+	fs.stat(__dirname + '/' + filename, function(err,stat){
+		stats[i] = stat;
+		if (stat.isDirectory()) {
+			console.log(i + '\033[36m' + filename + '/\033[39m');
+		}else{
+			console.log(i + '\033[90m' + filename + '/\033[39m');
+		}
+
+		i++;
+		if (i == files.length) {
+			read();
+		}else{
+			file(i);
+		}
+	});
+	}
+	function read(){
+	console.log('');
+	stdout.write('Enter your choice:');
+	stdin.resume();
+	stdin.setEncoding('utf-8');
+
+	stdin.on('data',option);
+	}
+	function option(data){
+		var filename = files[Number(data)];
+		if (!filename) {
+			stdout.write('Enter your choice:');
+		}else{
+			stdin.pause();
+			if (stats[Number(data)].isDirectory()) {
+				fs.readdir(__dirname + '/' + filename, function(err,files){
+					console.log('');
+					console.log('	(' + files.length + ' files)');
+					files.forEach(function(file){						//forEach函数。每个元数据都执行里面的匿名函数
+						console.log('	-	' + file);
+					});
+					console.log('');
+				});
+			}else{
+				fs.readFile(__dirname + '/' + filename, 'utf8', function(err,data){
+					console.log('');
+					console.log('\033[90m' + data.replace(/(.*)/g, '	$1') + '\033[39m'); 	//使用正则表达式进行辅助缩进
+				
+				});
+			}
+		}
+	}
+});
+
